4648ce7009e619c96d5cb58108ed2fb9b388e442
Jacob <jacob.22.g@gmail.com>
2020-12-05 16:24:47 +0200

    added email utills and cleaned the project

---
+++ CommonUtills/EmailUtills.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CommonUtills
+{
+    public class EmailUtills
+    {
+		// static regex
+		static string emailRegex = @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z";
+
+		// static is valid email
+		static public Boolean isEmailValid(string email) {
+			return Regex.IsMatch(email, emailRegex, RegexOptions.IgnoreCase);
+		}
+
+		// static Gmail sender - gets user name and password
+		static public Boolean GmailEmailSender(string reciverEmail, string subject, string body) {
+			if (!isEmailValid(reciverEmail))
+				return false;
+			else {
+
+				SmtpClient client = new SmtpClient();
+				client.Port = int.Parse(ConfigurationManager.AppSettings["SmtpClient_Port"]);
+				client.Host = ConfigurationManager.AppSettings["SmtpClient_Host"];
+				client.EnableSsl = Boolean.Parse(ConfigurationManager.AppSettings["SmtpClient_EnableSsl"]);
+				client.Timeout = int.Parse(ConfigurationManager.AppSettings["SmtpClient_Timeout"]);
+				client.DeliveryMethod = SmtpDeliveryMethod.Network;
+				client.UseDefaultCredentials = Boolean.Parse(ConfigurationManager.AppSettings["SmtpClient_UseDefaultCredentials"]);
+
+				client.Credentials = new System.Net.NetworkCredential(ConfigurationManager.AppSettings["Gmail_UserName"], ConfigurationManager.AppSettings["Gmail_Password"]);
+
+				MailMessage mm = new MailMessage("DoNotReply@domain.com", reciverEmail, subject, body);
+				mm.BodyEncoding = UTF8Encoding.UTF8;
+				mm.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+
+				client.Send(mm);
+
+				return true;
+			}
+		}
+    }
+}
--- WpfTask/View/SendEmail/SendEmailView.xaml.cs
+++ WpfTask/View/SendEmail/SendEmailView.xaml.cs
@@ -1,19 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Net.Mail;
-using System.Text.RegularExpressions;
+using System.Configuration;
+using CommonUtills;
 
 namespace WpfTask
 {
@@ -29,32 +20,10 @@ namespace WpfTask
 
 		private void sendButton_click(object sender, RoutedEventArgs e)
 		{
-			bool isValidEmail = Regex.IsMatch(email_TB.Text , @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", RegexOptions.IgnoreCase);
-
-			if (isValidEmail)
-			{
-				MessageBox.Show($"Email to { email_TB.Text } sent");
-
-
-				SmtpClient client = new SmtpClient();
-				client.Port = 587;
-				client.Host = "smtp.gmail.com";
-				client.EnableSsl = true;
-				client.Timeout = 10000;
-				client.DeliveryMethod = SmtpDeliveryMethod.Network;
-				client.UseDefaultCredentials = false;
-				client.Credentials = new System.Net.NetworkCredential("user.123.jacob@gmail.com", "User123123");
-
-				MailMessage mm = new MailMessage("DoNotReply@domain.com", email_TB.Text, body_TB.Text, subject_TB.Text);
-				mm.BodyEncoding = UTF8Encoding.UTF8;
-				mm.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
-
-				client.Send(mm);
-
-
+			if (EmailUtills.GmailEmailSender(email_TB.Text, subject_TB.Text, body_TB.Text)) {
+				MessageBox.Show($"Email sent to { email_TB.Text } successfully");
 			}
-			else
-			{
+			else {
 				MessageBox.Show($"Invalid Email");
 			}
 		}
